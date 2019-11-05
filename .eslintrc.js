3971591ba63fe96587c0499651b02d2eded4a2cc
Darren Lee <darren.lee@nexybit.com>
2019-11-05 17:04:16 +0900

    Setup husky for check code lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["@typescript-eslint", "prettier", "import-order"],
+  plugins: ['@typescript-eslint', 'prettier', 'import-order'],
   env: {
     browser: true,
     jasmine: true,
-    jest: true
+    jest: true,
   },
   rules: {
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/interface-name-prefix": 0,
-    "@typescript-eslint/no-empty-interface": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "no-confusing-arrow": ["error", { allowParens: true }],
-    "import/order/import-order": 2,
-    "react/prop-types": 0
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'no-confusing-arrow': ['error', { allowParens: true }],
+    // 'import-order': 1,
+    'react/prop-types': 0,
   },
   // settings: {
   //   react: {
@@ -31,5 +31,5 @@ module.exports = {
   //     }
   //   }
   // },
-  parser: "@typescript-eslint/parser"
+  parser: '@typescript-eslint/parser',
 };
